193f740210a3726d7fe7fbf6bb108c49953b0bf2
dudewithoneleg <galvancromeo@gmail.com>
2023-08-13 18:11:50 -0500

    login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
@@ -6,6 +8,10 @@ const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.get('/set-token-cookie', async (_req, res) => {
   const user = await User.findOne({
