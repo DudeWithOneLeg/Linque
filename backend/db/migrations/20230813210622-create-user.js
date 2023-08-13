176944e9397e9bf57b63d4f9a4eaa6b18ff7e35e
dudewithoneleg <galvancromeo@gmail.com>
2023-08-13 16:32:05 -0500

    users table

---
+++ backend/db/migrations/20230813210622-create-user.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+options.tableName = 'Users'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+
+  async up(queryInterface, Sequelize) {
+
+    await queryInterface.createTable(options, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull:false,
+        uniwue: true,
+        type: Sequelize.STRING(30)
+      },
+      email: {
+        allowNull:false,
+        uniwue: true,
+        type: Sequelize.STRING(265)
+      },
+      hashedPassword: {
+        allowNull:false,
+        type: Sequelize.STRING.BINARY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(options);
+  }
+};
