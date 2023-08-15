939bef4740c266862cd0d1065fe0664428fb3219
dudewithoneleg <galvancromeo@gmail.com>
2023-08-14 23:03:47 -0500

    model names

--- backend/db/migrations/20230814185343-create-user-settings.js
+++ backend/db/migrations/20230814185343-create-user-settings.js
@@ -1,7 +1,6 @@
 'use strict';
 
 
-
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production') {
@@ -9,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 module.exports = {
   async up(queryInterface, Sequelize) {
-    
+
     await queryInterface.createTable("UserSettings", {
       id: {
         allowNull: false,
