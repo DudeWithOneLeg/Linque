01cefb77158efc2eac1c0b050ca6ab181348043f
dudewithoneleg <galvancromeo@gmail.com>
2023-08-14 21:54:05 -0500

    model names

--- backend/db/migrations/20230814185343-create-user-setting.js
+++ backend/db/migrations/20230814185343-create-user-setting.js
@@ -1,13 +1,13 @@
 'use strict';
 
 
-options.tableName = 'UserSettings'
 
 /** @type {import('sequelize-cli').Migration} */
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
+options.tableName = 'UserSettings'
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("UserSettings", {
