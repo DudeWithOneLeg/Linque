b2fb52b7eb723672d534ce24005a92fe39d163fe
dudewithoneleg <galvancromeo@gmail.com>
2023-08-14 20:19:35 -0500

    render build script

--- backend/db/models/chatbotmessage.js
+++ backend/db/models/chatbotmessage.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      ChatBotMessage.belongsTo(models.chatBotConvo, {
+      ChatBotMessage.belongsTo(models.ChatBotConvo, {
         foreignKey: 'chatBotConvoId'
       })
     }
