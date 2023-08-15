9987d334983cf1595427bd54693c7ed9ce2659c8
dudewithoneleg <galvancromeo@gmail.com>
2023-08-14 21:34:55 -0500

    model names

--- backend/db/migrations/20230814185343-create-user-setting.js
+++ backend/db/migrations/20230814185343-create-user-setting.js
@@ -41,7 +41,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.literal('CURRENT_TIMESTAMP'),
       }
-    }, options);
+    });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable(options);
