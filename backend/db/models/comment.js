33217c4864d1256ca86db1c28c6cc286f8c92aab
dudewithoneleg <galvancromeo@gmail.com>
2023-08-14 23:11:01 -0500

    testing db prod error

--- backend/db/models/comment.js
+++ backend/db/models/comment.js
@@ -11,9 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Comment.belongsTo(models.Post, {
-        foreignKey: 'postId'
-      })
+      
     }
   }
   Comment.init({
--- backend/db/models/postimage.js
+++ backend/db/models/postimage.js
@@ -11,9 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      PostImage.belongsTo(models.Post, {
-        foreignKey: 'postId'
-      })
+      
     }
   }
   PostImage.init({
