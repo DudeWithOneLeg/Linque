a0eb750f67b193fffa73aa11e17ddcfb607822d8
dudewithoneleg <galvancromeo@gmail.com>
2023-08-14 22:48:41 -0500

    model names

---
+++ backend/db/models/usersetting.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class UserSetting extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      UserSetting.belongsTo(models.User, {
+        foreignKey: 'userId'
+      })
+    }
+  }
+  UserSetting.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.DECIMAL,
+      allowNull: true
+    },
+    lng: {
+      type: DataTypes.DECIMAL,
+      allowNull: true
+    },
+    chatAudioResponse: {
+      type: DataTypes.BOOLEAN
+    }
+  }, {
+    sequelize,
+    modelName: 'UserSetting'
+  });
+  return UserSetting;
+};
