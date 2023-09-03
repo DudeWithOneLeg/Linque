40c2f61ee23c700653e6fd9ae09a31d9667020be
dudewithoneleg <galvancromeo@gmail.com>
2023-09-03 05:32:16 -0500

    chat bot

---
+++ backend/db/migrations/20230815042713-create-user-convos.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+const options = {}
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+options.tableName = 'UserConvos'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('UserConvos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      friendshipId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'UserConvos'
+    await queryInterface.dropTable(options);
+  }
+};
