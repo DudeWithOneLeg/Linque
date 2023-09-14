84054a0300c2c88606a4a4b9edf7305fc4e98791
dudewithoneleg <galvancromeo@gmail.com>
2023-09-14 18:41:35 -0500

    fixed production socket url

--- backend/app.js
+++ backend/app.js
@@ -93,9 +93,9 @@ const server = http.createServer(app);
 
 const isProduction = environment === 'production';
 let url = 'http://localhost:3000'
-if (isProduction) {
-  url = `https://linque.onrender.com`
-}
+// if (isProduction) {
+//   url = `https://linque.onrender.com`
+// }
 const io = require("socket.io")(server, {
   cors: {
     origin: url,
